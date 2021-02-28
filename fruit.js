9e137dfbfba57338010d716133b68e7df09efe4b
Chris <51548719+ZoologyJS@users.noreply.github.com>
2021-02-27 17:56:11 -0800

    Update fruit.js

--- fruit.js
+++ fruit.js
@@ -2,4 +2,4 @@ document.write(~~!!this,"\025",(btoa("l").substring(0,2)+ +`
 
 What is the perfect number of fruit?
 
-`+(+void(eval)+"")[1]).toLowerCase());
+`+(+void(/huh?/)+"")[1]).toLowerCase());
